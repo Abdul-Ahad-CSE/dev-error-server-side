2cac486b975310dec0822cc43111805f75c6f467
Abdul-Ahad-CSE <abdulahad.cse1@gmail.com>
2026-05-25 23:41:19 +0600

    Project name updated

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.get("/", (req: Request, res: Response) => {
   //res.send("Hello World!");
   res.status(200).json({
     message: "Express Server",
-    author: "DevPulse",
+    author: "DevError",
   });
 });
 
